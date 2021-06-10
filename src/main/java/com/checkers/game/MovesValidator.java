e2a2f79449201ee2e434962f4f523692977abb80
SergeySmirnovJava <sergej.smirnov1998@yandex.ru>
2021-06-10 13:55:19 +0300

    Fix method for validate moves

--- src/main/java/com/checkers/game/MovesValidator.java
+++ src/main/java/com/checkers/game/MovesValidator.java
@@ -7,39 +7,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MovesValidator {
-    private ArrayList<String> whiteCheckers;
-    private ArrayList<String> blackCheckers;
-    private CheckerGame checker;
+    private final CheckerGame checker;
     private final Pattern startPositions;
+    private final Pattern moveType;
     private Matcher matchPositions;
 
     public MovesValidator(String whiteCheckers, String blackCheckers){
         startPositions = Pattern.compile("[a-hA-h][1-8]");
+        moveType = Pattern.compile("[:||-]");
         matchPositions = startPositions.matcher(whiteCheckers);
+        ArrayList<String> whiteCheckers1 = new ArrayList<>();
         while (matchPositions.find()){
-            this.whiteCheckers.add(whiteCheckers.substring(matchPositions.start(), matchPositions.end()));
+            whiteCheckers1.add(whiteCheckers.substring(matchPositions.start(), matchPositions.end()));
         }
         matchPositions = startPositions.matcher(blackCheckers);
+        ArrayList<String> blackCheckers1 = new ArrayList<>();
         while (matchPositions.find()){
-            this.blackCheckers.add(blackCheckers.substring(matchPositions.start(), matchPositions.end()));
+            blackCheckers1.add(blackCheckers.substring(matchPositions.start(), matchPositions.end()));
         }
-        checker = new CheckerGame(this.whiteCheckers, this.blackCheckers);
+        checker = new CheckerGame(whiteCheckers1, blackCheckers1);
     }
 
     public void handleMove(String moves) throws Exception {
         matchPositions = startPositions.matcher(moves);
-        if(matchPositions.find()) {
+        Matcher matchType = moveType.matcher(moves);
+
+        while(matchPositions.find()) {
             checker.setCurrentCell(moves.substring(matchPositions.start(), matchPositions.end()));
-            if (moves.contains("-")) {
-                if(matchPositions.find()) {
-                    checker.regularMove(moves.substring(matchPositions.start(), matchPositions.end()));
-                }
-            } else {
-                while(matchPositions.find()) {
-                    checker.attackMove(moves.substring(matchPositions.start(), matchPositions.end()));
+            if(matchType.find()) {
+                if (moves.substring(matchType.start(), matchType.end()).equals("-")) {
+                    if (matchPositions.find()) {
+                        checker.regularMove(moves.substring(matchPositions.start(), matchPositions.end()));
+                    }
+                } else{
+                    if (matchPositions.find()) {
+                        checker.attackMove(moves.substring(matchPositions.start(), matchPositions.end()));
+                    }
                 }
             }
+            checker.swapCheckers();
         }
-        checker.swapCheckers();
+    }
+
+    public String toString(){
+        return checker.toString();
     }
 }
