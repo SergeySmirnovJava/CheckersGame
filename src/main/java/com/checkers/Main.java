99da37bad888a10ded3fd7b3ce3e8e717964a76f
SergeySmirnovJava <sergej.smirnov1998@yandex.ru>
2021-06-10 13:54:34 +0300

    main method for checkers game

--- src/main/java/com/checkers/Main.java
+++ src/main/java/com/checkers/Main.java
@@ -1,7 +1,31 @@
 package com.checkers;
 
+import com.checkers.game.MovesValidator;
+import com.checkers.game.SyntaxValidator;
+import com.checkers.moves.CheckerGame;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        List<String> checkers = Files.readAllLines(Paths.get("moves.txt"));
+        String whiteCheckers = checkers.get(0);
+        String blackCheckers = checkers.get(1);
+        SyntaxValidator syntaxValidator = new SyntaxValidator();
+        MovesValidator movesValidator = new MovesValidator(whiteCheckers, blackCheckers);
+        checkers = checkers.subList(2, checkers.size());
+        String tempMove;
+        for (String moves : checkers){
+            tempMove = syntaxValidator.handleMove(moves);
+            movesValidator.handleMove(tempMove);
+        }
+        System.out.println(movesValidator);
 
     }
 }
